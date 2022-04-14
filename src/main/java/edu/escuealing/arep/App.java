b7fbb26fb33a14f0c48b9cb68355e7612bfbd8ff
Juan Sebastian Cadavid <juan.cadavid-p@mail.escuelaing.edu.co>
2022-04-14 14:06:03 -0500

    Inicial

---
+++ src/main/java/edu/escuealing/arep/App.java
@@ -0,0 +1,24 @@
+package edu.escuealing.arep;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Spark");
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
