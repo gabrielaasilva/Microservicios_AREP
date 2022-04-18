f4d337e98829c1c5bf0fba52f66d5f7e22f2f57a
Juank544 <camilo.gv@live.com>
2022-04-18 14:49:01 -0500

    Basic Spark

--- Feed/src/main/java/co/edu/escuelaing/App.java
+++ Feed/src/main/java/co/edu/escuelaing/App.java
@@ -1,5 +1,8 @@
 package co.edu.escuelaing;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        port(getport());
+        get("/feed", (request,response) -> "Aqui va el feed");
         System.out.println( "Hello World!" );
+    }
+
+    private static int getport(){
+        if (System.getenv("PORT") != null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4568;
     }
 }
