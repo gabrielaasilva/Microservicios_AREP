e1dcf68a26c9b782982168405e943dee5ab5f51b
Juan Sebastian Cadavid <juan.cadavid-p@mail.escuelaing.edu.co>
2022-04-18 18:15:59 -0500

    Post DataDB

--- Client/src/main/resources/script.js
+++ Client/src/main/resources/script.js
@@ -9,3 +9,15 @@ function cuenta (campo, limite) {
         x.querySelector(".example").innerHTML = (limite - campo.value.length);
     }    
 }
+async function enviarPost () {
+    post = document.getElementsByName("mensaje")[0].value;
+    res = {post}
+
+    const date = await fetch(`http://localhost:4569/post`, {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(res)
+    });
+}
