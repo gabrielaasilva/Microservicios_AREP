e68698d9263b70c8ff3724582474d9495b184732
Juank544 <camilo.gv@live.com>
2022-04-20 19:35:47 -0500

    Allow CORS

--- Feed/src/main/java/co/edu/escuelaing/App.java
+++ Feed/src/main/java/co/edu/escuelaing/App.java
@@ -14,6 +14,24 @@ public class App
     public static void main( String[] args )
     {
         port(getport());
+        staticFileLocation("/");
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         path("/feed", () -> {
             get("/20", (request,response) -> {
                 response.type("application/json");
